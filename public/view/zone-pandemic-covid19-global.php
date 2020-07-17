2e3d2c8d08ee72c272a5ccbd25ef36051691d8bb
zekinah <zjlecaros@gmail.com>
2020-07-17 16:20:12 -0700

    Fixed Styles Global

--- public/view/zone-pandemic-covid19-global.php
+++ public/view/zone-pandemic-covid19-global.php
@@ -17,77 +17,77 @@
 <section id="zn-covid19" class="section">
     <div class="zn-loading"><span class="covid19">🦠</span></div>
     <div class="zn-covid19__content">
-        <div class="title has-text-centered">
-            <h2 id="zn-global-cases" class="is-2"></h2>
-            <h4 class="is-4">🌎 <?= __('Global Total') ?></h4>
+        <div class="has-text-centered mb-2">
+            <p id="zn-global-cases" class="title is-1"></p>
+            <p class="subtitle is-4">🌎 <?= __('Global Total') ?></p>
         </div>
-        <div><p class="subtitle is-4"><?= __('Affected Countries: ') ?><span id="zn-global-affected-countries"></span></p></div>
-        <div><p class="subtitle is-4"><?= __('Last update on: ') ?><span id="zn-global-updates"></span></p></div>
-        <div class="columns is-desktop">
-            <div class="column">
+        <div><p class="subtitle is-6"><?= __('Affected Countries: ') ?><span id="zn-global-affected-countries"></span></p></div>
+        <div><p class="subtitle is-6"><?= __('Last update on: ') ?><span id="zn-global-updates"></span></p></div>
+        <div class="columns is-multiline is-desktop mt-1">
+            <div class="column is-6-desktop">
                 <div class="card">
                     <div class="card-content">
                         <div class="zn-title">
                             <div class="emoji mr-2">🤧</div>
                             <div class="zn-subtitle">
-                                <h2 class="title is-3"><?= __('Active') ?></h2>
-                                <h5 class="subtitle is-6">(<?= __('+onePerMillion') ?>)</h5>
+                                <p class="title is-5"><?= __('Active') ?></p>
+                                <p class="subtitle is-6">(<?= __('+onePerMillion') ?>)</p>
                             </div>
                         </div>
                         <div class="zn-statistics">
-                            <h3 id="zn-global-active" class="title is-3"></h3>
-                            <h5 class="subtitle is-5">(+<span id="zn-global-activePerM"></span>)</h5>
+                            <p id="zn-global-active" class="title is-3"></p>
+                            <p class="subtitle is-5">(+<span id="zn-global-activePerM"></span>)</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="column">
+            <div class="column is-6-desktop">
                 <div class="card">
                     <div class="card-content">
                         <div class="zn-title">
                             <div class="emoji mr-2">😄</div>
                             <div class="zn-subtitle">
-                                <h2 class="title is-3"><?= __('Recovered') ?></h2>
-                                <h5 class="subtitle is-6">(<?= __('+onePerMillion') ?>)</h5>
+                                <p class="title is-5"><?= __('Recovered') ?></p>
+                                <p class="subtitle is-6">(<?= __('+onePerMillion') ?>)</p>
                             </div>
                         </div>
                         <div class="zn-statistics">
-                            <h3 id="zn-global-recovered" class="title is-3"></h3>
-                            <h5 class="subtitle is-5">(+<span id="zn-global-recoveredPerM"></span>)</h5>
+                            <p id="zn-global-recovered" class="title is-3"></p>
+                            <p class="subtitle is-5">(+<span id="zn-global-recoveredPerM"></span>)</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="column">
+            <div class="column is-6-desktop">
                 <div class="card">
                     <div class="card-content">
                         <div class="zn-title">
                             <div class="emoji mr-2">😷</div>
                             <div class="zn-subtitle">
-                                <h2 class="title is-3"><?= __('Critical') ?></h2>
-                                <h5 class="subtitle is-6">(<?= __('+onePerMillion') ?>)</h5>
+                                <p class="title is-5"><?= __('Critical') ?></p>
+                                <p class="subtitle is-6">(<?= __('+onePerMillion') ?>)</p>
                             </div>
                         </div>
                         <div class="zn-statistics">
-                            <h3 id="zn-global-critical" class="title is-3"></h3>
-                            <h5 class="subtitle is-5">(+<span id="zn-global-criticalPerM"></span>)</h5>
+                            <p id="zn-global-critical" class="title is-3"></p>
+                            <p class="subtitle is-5">(+<span id="zn-global-criticalPerM"></span>)</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="column">
+            <div class="column is-6-desktop">
                 <div class="card">
                     <div class="card-content">
                     <div class="zn-title">
                             <div class="emoji mr-2">💀</div>
                             <div class="zn-subtitle">
-                                <h2 class="title is-3"><?= __('Deaths') ?></h2>
-                                <h5 class="subtitle is-6">(<?= __('+onePerMillion') ?>)</h5>
+                                <p class="title is-5"><?= __('Deaths') ?></p>
+                                <p class="subtitle is-6">(<?= __('+onePerMillion') ?>)</p>
                             </div>
                         </div>
                         <div class="zn-statistics">
-                            <h3 id="zn-global-deaths" class="title is-3"></h3>
-                            <h5 class="subtitle is-5">(+<span id="zn-global-deathsPerM"></span>)</h5>
+                            <p id="zn-global-deaths" class="title is-3"></p>
+                            <p class="subtitle is-5">(+<span id="zn-global-deathsPerM"></span>)</p>
                         </div>
                     </div>
                 </div>
