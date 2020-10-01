fb67cdfd4080e051eae334f2e0a38e023dbb7a11
zekinah <zjlecaros@gmail.com>
2020-09-30 20:20:26 -0700

    CSS Tweaks

--- admin/js/pandemic-covid19-admin.js
+++ admin/js/pandemic-covid19-admin.js
@@ -3,6 +3,11 @@
 	$ = jQuery.noConflict();
 	$( window ).load(function() {
 		$('#tbl-countries').DataTable();
+
+		$('.txt-shortcode').on("click", function () {
+			$(this).focus();
+			$(this).select();
+		});
 	});
 
 })( jQuery );
