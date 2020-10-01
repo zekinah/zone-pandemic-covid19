e37db388db14c338d367a68ac9a1fd7b34aaccc6
zekinah <zjlecaros@gmail.com>
2020-09-30 20:21:45 -0700

    Released version 1.0.8

--- includes/class-pandemic-covid19.php
+++ includes/class-pandemic-covid19.php
@@ -75,7 +75,7 @@ class Pandemic_Covid19 {
 		if ( defined( 'PANDEMIC_COVID19_VERSION' ) ) {
 			$this->version = PANDEMIC_COVID19_VERSION;
 		} else {
-			$this->version = '1.0.7';
+			$this->version = '1.0.8';
 		}
 		$this->plugin_name = 'pandemic-covid19';
 
