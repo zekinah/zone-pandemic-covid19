087d00669f7eeb37ece8a7bb9192c7b8e756f5d1
zekinah <zjlecaros@gmail.com>
2021-05-06 22:17:16 +0800

    Release 1.1.0

--- pandemic-covid19.php
+++ pandemic-covid19.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Zone Pandemic Covid-19
  * Plugin URI:        https://github.com/zekinah/wp-pandemic-covid19
  * Description:       This plugin provides shortcode that displays the live recorded data of the covid19 in the whole world.
- * Version:           1.0.9
+ * Version:           1.1.0
  * Author:            Zekinah Lecaros
  * Author URI:        zekinahlecaros.com
  * License:           GPL-2.0+
